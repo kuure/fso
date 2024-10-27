59b5e385621ad949f688b5d7904276da0b534517
Jeff Kuure <jeffkuure@gmail.com>
2024-10-27 16:24:37 -0400

    added input handlers, comments, more logging

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,58 @@ import { useState } from 'react'
 
 const App = () => {
 
+	// data, 'persons' array
 	const [persons, setPersons] = useState([
 		{ name: 'Arto Hellas' }
 	]) 
 
+	// new name state
 	const [newName, setNewName] = useState('')
 
 
 	// add a new person
 	const addPerson = (event) => {
+
+		console.log('addPerson event', event)
 		event.preventDefault()
+
 		const personObject = {
 			content: newName,
 			id: String(persons.length + 1),
 		}
+
+		console.log(persons)
 		setPersons(persons.concat(personObject))
 		setNewName('')
+		console.log(persons)
+
 	}
 
 
-
-
-
-
+	const handlePersonChange = () => {
+		console.log(event.target.value)
+		setNewName(event.target.value)
+	}
 
 	return (
 		<div>
+
 			<h2>Phonebook</h2>
 
 			<form onSubmit={addPerson}>
+
 				<div>
-					name: <input />
+					name :
+					<input
+						value={newName}
+						onChange={handlePersonChange}
+					/>
 				</div>
+
 				<div>
 					<button type="submit">add</button>
 				</div>
+
 			</form>
 
 			<h2>Numbers</h2>
