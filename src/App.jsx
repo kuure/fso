1a593bc2a90c948c0cec10e04933601bce6b4d5e
kuure <kuure@msu.edu>
2024-10-27 12:37:59 -0400

    working version after 'saving the notes' step

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,37 @@
-import Course from "./components/Course"
+import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-	const courses = [
-		{
-			name: 'Half Stack application development',
-			id: 1,
-			parts: [
-				{
-					name: 'Fundamentals of React',
-					exercises: 10,
-					id: 1
-				},
-				{
-					name: 'Using props to pass data',
-					exercises: 7,
-					id: 2
-				},
-				{
-					name: 'State of a component',
-					exercises: 14,
-					id: 3
-				},
-				{
-					name: 'Redux',
-					exercises: 11,
-					id: 4
-				}
-			]
-		},
-		{
-			name: 'Node.js',
-			id: 2,
-			parts: [
-				{
-					name: 'Routing',
-					exercises: 3,
-					id: 1
-				},
-				{
-					name: 'Middlewares',
-					exercises: 7,
-					id: 2
-				}
-			]
-		}
-	]
+
+const App = (props) => {
+
+	const [notes, setNotes] = useState(props.notes)
+
+
+	const addNote = (event) => {
+		event.preventDefault()
+		console.log('button clicked', event.target)
+	}
+
 
 	return (
-		<Course courses={courses}/>
-  )
+		<div>
+
+			<h1>Notes</h1>
+
+			<ul>
+				{notes.map(note => 
+					<Note key={note.id} note={note} />
+				)}
+			</ul>
+
+			<form onSubmit={addNote}>
+				<input />
+				<button type="submit">save</button>
+			</form>
+			
+
+		</div>
+	)
 }
-export default App
+
+export default App 
