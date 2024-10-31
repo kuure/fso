067adfe8a5fd1e2ceea948cfb01de261d1cd5011
kuure <kuure@msu.edu>
2024-10-31 15:22:54 -0400

    not quite working version of 2.9

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Person from './components/Person'
 const App = () => {
 
 	// data, 'persons' array
-	const [persons, setPersons] = useState([
+  const [persons, setPersons] = useState([
 		{ name: 'jeff', number: "6307" },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+
 	]) 
 
 	// new name state
@@ -13,6 +18,9 @@ const App = () => {
 	
 	// new number state
 	const [newNumber, setNewNumber] = useState('')
+	
+	// new filter state
+	const [newFilter, setNewFilter] = useState('')
 
 
 	// add a new person
@@ -40,14 +48,25 @@ const App = () => {
 		}
 	}
 
-	// get the values from the name and number fields
+	// event handlers
 	const handleNameChange = () => setNewName(event.target.value)
 	const handleNumberChange = () => setNewNumber(event.target.value)
+	const handleFilterChange = () => setNewFilter(event.target.value)
+
+
+	const personsToShow = newName ? persons.filter(person => person.name.toLowerCase().includes(newName.toLowerCase())) : persons 
+	console.log(personsToShow)
+	console.log(newName)
+
 
 	// GUI stuff
 	return (
 		<div>
+
 			<h2>Phonebook</h2>
+			<div>
+				filter: <input value={newFilter} onChange={handleFilterChange}/>
+			</div>
 			<form onSubmit={addPerson}>
 				<div>name : <input value={newName} onChange={handleNameChange} /> </div>
 				<div>number: <input value={newNumber} onChange={handleNumberChange} /> </div>
@@ -58,9 +77,7 @@ const App = () => {
 
 			<h2>Numbers</h2>
 			<ul>
-				{persons.map(person => 
-					<Person key={person.name} person={person} />
-				)}
+				{ personsToShow.map(person => <Person key={person.name} person={person} />)}
 			</ul>
 
 			<div>debug: {newNumber}</div>
