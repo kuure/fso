1618abdde0283c7a0fb5ad2ae19190f924d1031d
kuure <kuure@msu.edu>
2024-11-03 11:07:15 -0500

    switched back to PhonebookApp.jsx as the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,94 @@
 import { useState,useEffect } from 'react'
-import Note from './components/Note'
-import noteService from './services/notes'
-
+import axios from 'axios'
+import Filter from './components/Filter'
 
 const App = () => {
 
-	// note state
-	const [notes, setNotes] = useState([])
-	// new note state
-	const [newNote, setNewNote] = useState('')
-	// determine what ones to show
-	const [showAll,setShowAll] = useState(true)
+	// data, 'persons' array
+  const [persons, setPersons] = useState([]) 
 
-	// get the data right when the page loads
+	// new name state
+	const [newName, setNewName] = useState('')
+	
+	// new number state
+	const [newNumber, setNewNumber] = useState('')
+	
+	// new filter state
+	const [newFilter, setNewFilter] = useState('')
+
 	useEffect(() => {
-		noteService
-			.getAll()
-			.then(initialNotes => {
-				setNotes(initialNotes)
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				console.log(response.data)
+				setPersons(response.data)
 			})
-	}, [])
+	},[])
 
-	// add a new note
-	const addNote = (event) => {
+
+	// add a new person
+	const addPerson = (event) => {
 		event.preventDefault()
-		const noteObject = {
-			content: newNote,
-			important: Math.random() < 0.5,
+
+		// template for person
+		const personObject = {
+			name: newName,
+			number: newNumber,
+			id: String(persons.length + 1),
 		}
-		noteService
-			.create(noteObject)
-			.then(returnedNote => {
-				setNotes(notes.concat(returnedNote))
-				setNewNote('')
-			})
+
+		// is there a person in the book with the same name?
+		const personMatch = persons.some(person => person.name === newName)
+		if (personMatch) {
+			window.confirm( `${newName} is already added to the phonebook`)
+		}
+		else {
+			// change the state for newName and newNumber
+			setNewName(newName)
+			setNewNumber(newNumber)
+			// add the personObject to the persons array
+			setPersons(persons.concat(personObject))
+		}
+	}
+
+	// event handlers
+	const handleNameChange = () => {
+		console.log(event.target)
+		setNewName(event.target.value)
 	}
 
 
-	// change importance of a note
-	const toggleImportanceOf = (id) => {
-		const note = notes.find(n => n.id === id)
-		// the spread operator
-		const changedNote = { ...note, important: !note.important }
-
-		noteService
-			.update(id, changedNote)
-			.then(returnedNote => {
-				setNotes(notes.map(note => note.id===id ? returnedNote : note ))
-			})
-			.catch(error => {
-				alert(`the note '${note.content}' was already deleted from the server`)
-				setNotes(notes.filter(n => n.id !== id))
-			})
-	}
-
-
-
-	// deal with edits
-	const handleNoteChange = (event) => {
-		console.log(event.target.value)
-		setNewNote(event.target.value)
-	}
-
-	// which notes to display?
-	const notesToShow = showAll
-		? notes
-		: notes.filter(note => note.important)
-
+	const handleNumberChange = () => setNewNumber(event.target.value)
+	const handleFilterChange = () => setNewFilter(event.target.value)
 
 
 	// GUI stuff
 	return (
 		<div>
 
-			<h1>Notes</h1>
+			<h2>Phonebook</h2>
+			
 
 			<div>
-				<button onClick={() => setShowAll(!showAll)}>
-					show {showAll ? 'important' : 'all'}
-				</button>
+				filter: <input value={newFilter} onChange={handleFilterChange}/>
 			</div>
-	
-			<ul>
-				{notesToShow.map(note =>
-					<Note
-						key={note.id}
-						note={note}
-						toggleImportance={() => toggleImportanceOf(note.id)}
-					/>
-				)}
-			</ul>
-
-			<form onSubmit={addNote}>
-
-				<input
-					value={newNote}
-					onChange={handleNoteChange}
-				/>
-
-				<button type="submit">save</button>
-
+			<form onSubmit={addPerson}>
+				<div>name : <input value={newName} onChange={handleNameChange} /> </div>
+				<div>number: <input value={newNumber} onChange={handleNumberChange} /> </div>
+				<div>
+					<button type="submit">add</button>
+				</div>
 			</form>
+
+			<h2>Numbers</h2>
+			<ul>
+				<Filter persons={persons} name={newFilter} />
+			</ul>
 
 		</div>
 	)
 }
+export default App
 
-export default App 
+
+
