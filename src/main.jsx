ccc954b0a1043c01fe9148bec3e5dfff08330f70
kuure <kuure@msu.edu>
2024-11-19 09:41:24 -0500

    added currency exchange app, moved css to subfolder

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-import './reset.css'
-import './index.css'
+import './css/reset.css'
+import './css/index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
